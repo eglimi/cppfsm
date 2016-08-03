65b5eb634c950c694eb6a89e6e593510426390f7
Michael Egli <michael.egli@wisol.ch>
2016-08-03 22:14:58 +0200

    Remove leading stars in the file documentation.
    The leading stars add nothing for the file documentation, and it
    is less distracting to read without them, IMO. Doxygen handles the
    comments fine as well.

--- fsm.h
+++ fsm.h
@@ -2,171 +2,171 @@
 #define FSM_H
 
 /**
- * The MIT License (MIT)
- *
- * Copyright (c) 2014 Michael Egli
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * \author    Michael Egli
- * \copyright Michael Egli
- * \date      18-Dec-2014
- * \file fsm.h
- *
- * Finite State Machine
- * ====================
- *
- * Generic implementation of a finite state machine (FSM).
- *
- * Overview
- * --------
- *
- * A finite state machine is a machine that can be in any of a finite number of
- * states. At a time, it can only be in one state. In order to change the
- * machine to another state, a transition can be executed.
- *
- * A transition is initiated by a trigger. A trigger to the machine is only
- * evaluated for outgoing transitions of the current state. A transition can
- * have a guard and a transition action associated with it. The guard is a
- * function that checks if the transition should be executed. The transition
- * action is a function that is called when the transition is effectively
- * executed.
- *
- * If a trigger is sent to the machine, and more than one guard evaluate to
- * `true`, then one of the transitions is randomly chosen.
- *
- * A state can have any number of incoming and / or outgoing transitions
- * associated with it, but at least one transition must be present. There are
- * two special states.
- *
- * - Initial pseudo state (no incoming transition, one or more outgoing
- *   transitions).
- * - Final pseudo state (one or more incoming transitions, no outgoing
- *   transition).
- *
- * The initial pseudo state must be present in each FSM.
- *
- * Important concepts
- * ------------------
- *
- * Some concepts are important to remember when defining a state machine.
- *
- * - Each FSM must define a transition from the initial pseudo state to
- *   another state.
- * - If a guard is executed multiple times successively, it must return the
- *   same value every time (no side-effects in guards).
- * - A FSM is a purely reactive, and therefore passive component. In order to
- *   execute some actions, it must receive a trigger.
- *
- * Semantics
- * ---------
- *
- * The following semantic is implemented when a machine receives a trigger.
- *
- * - Consume a trigger.
- * - Check if the trigger potentially initiates some transitions from the
- *   current state. Otherwise return.
- * - For each transition found, check if its guard evaluates to `true`. Return
- *   if none is found.
- * - Execute the transition action of one (and only one) of the selected
- *   transition.
- * - Change current state to the state where the transition points to.
- *
- * Limitations
- * -----------
- *
- * This FSM implementation does not implement all concepts found in all
- * definitions of state machines (notably the UML definition). Some parts that
- * are omitted are.
- *
- * - entry actions, exit actions, do actions
- * - hierarchical nested states
- * - orthogonal regions
- *
- * Implementation
- * --------------
- *
- * This implementation can be used to implement a FSM. It enforces the
- * semantics described above, while being non-intrusive. All guards and actions
- * are defined in the client.
- *
- * The state machine and transitions can be conveniently defined with an array
- * of FSM::Trans structs. This makes the structure of the FSM very clear.
- *
- * C++11
- * -----
- *
- * The implementation uses some C++11 features. Therefore, in order to use the
- * code, the compiler must support theses features and C++11 must be enabled.
- *
- * Debug
- * -----
- *
- * It is possible to add a debug function in order to track state changes. The
- * debug function is either a `nullptr`, or of type `debugFn`. When the
- * function is defined, it is invoked with the `from_state`, `to_state`, and
- * `trigger` arguments whenever a state change happens.
- *
- * ~~~
- * // Define debug function
- * void dbg_fsm(State from_state, State to_state, Trigger trigger) {
- *   std::cout << "state has changed\n";
- * }
- * // Enable debug
- * fsm.add_debug_fn(dbg_fsm);
- * // Disable debug
- * fsm.add_debug_fn(nullptr);
- * ~~~
- *
- * Example
- * -------
- *
- * The following example implements this simple state machine.
- *
- * ~~~
- *   +----------+                    +--------+                              +---------+
- *   | Initial  | -- 'a'/action1 --> | stateA | -- [guard2]'b' / action2 --> | Final   |
- *   +----------+                    +--------+                              +---------+
- * ~~~
- *
- * ~~~
- *   void action1() { std::cout << "perform custom action 1\n"; }
- *   void action2() { std::cout << "perform custom action 2\n"; }
- *   enum class States {Initial, A, Final};
- *   using F = FSM::Fsm<States, States::Initial>;
- *   std::vector<F::Trans> transitions =
- *   {
- *     // from state     , to state      , trigger, guard           , action
- *     { States::Initial , States::A     , 'a'    , nullptr         , action1 },
- *     { States::A       , States::Final , 'b'    , []{return true} , action2 },
- *   };
- *
- *   F fsm;
- *   fsm.add_transitions(transitions);
- *   assert(is_initial());
- *   fsm.execute('a');
- *   assert(States::A == fsm.state());
- *   fsm.execute('b');
- *   assert(States::Final == fsm.state());
- *   fsm.reset();
- *   assert(is_initial());
- * ~~~
+The MIT License (MIT)
+
+Copyright (c) 2014 Michael Egli
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+\author    Michael Egli
+\copyright Michael Egli
+\date      18-Dec-2014
+\file fsm.h
+
+Finite State Machine
+====================
+
+Generic implementation of a finite state machine (FSM).
+
+Overview
+--------
+
+A finite state machine is a machine that can be in any of a finite number of
+states. At a time, it can only be in one state. In order to change the
+machine to another state, a transition can be executed.
+
+A transition is initiated by a trigger. A trigger to the machine is only
+evaluated for outgoing transitions of the current state. A transition can
+have a guard and a transition action associated with it. The guard is a
+function that checks if the transition should be executed. The transition
+action is a function that is called when the transition is effectively
+executed.
+
+If a trigger is sent to the machine, and more than one guard evaluate to
+`true`, then one of the transitions is randomly chosen.
+
+A state can have any number of incoming and / or outgoing transitions
+associated with it, but at least one transition must be present. There are
+two special states.
+
+- Initial pseudo state (no incoming transition, one or more outgoing
+  transitions).
+- Final pseudo state (one or more incoming transitions, no outgoing
+  transition).
+
+The initial pseudo state must be present in each FSM.
+
+Important concepts
+------------------
+
+Some concepts are important to remember when defining a state machine.
+
+- Each FSM must define a transition from the initial pseudo state to
+  another state.
+- If a guard is executed multiple times successively, it must return the
+  same value every time (no side-effects in guards).
+- A FSM is a purely reactive, and therefore passive component. In order to
+  execute some actions, it must receive a trigger.
+
+Semantics
+---------
+
+The following semantic is implemented when a machine receives a trigger.
+
+- Consume a trigger.
+- Check if the trigger potentially initiates some transitions from the
+  current state. Otherwise return.
+- For each transition found, check if its guard evaluates to `true`. Return
+  if none is found.
+- Execute the transition action of one (and only one) of the selected
+  transition.
+- Change current state to the state where the transition points to.
+
+Limitations
+-----------
+
+This FSM implementation does not implement all concepts found in all
+definitions of state machines (notably the UML definition). Some parts that
+are omitted are.
+
+- entry actions, exit actions, do actions
+- hierarchical nested states
+- orthogonal regions
+
+Implementation
+--------------
+
+This implementation can be used to implement a FSM. It enforces the
+semantics described above, while being non-intrusive. All guards and actions
+are defined in the client.
+
+The state machine and transitions can be conveniently defined with an array
+of FSM::Trans structs. This makes the structure of the FSM very clear.
+
+C++11
+-----
+
+The implementation uses some C++11 features. Therefore, in order to use the
+code, the compiler must support theses features and C++11 must be enabled.
+
+Debug
+-----
+
+It is possible to add a debug function in order to track state changes. The
+debug function is either a `nullptr`, or of type `debugFn`. When the
+function is defined, it is invoked with the `from_state`, `to_state`, and
+`trigger` arguments whenever a state change happens.
+
+~~~
+// Define debug function
+void dbg_fsm(State from_state, State to_state, Trigger trigger) {
+  std::cout << "state has changed\n";
+}
+// Enable debug
+fsm.add_debug_fn(dbg_fsm);
+// Disable debug
+fsm.add_debug_fn(nullptr);
+~~~
+
+Example
+-------
+
+The following example implements this simple state machine.
+
+~~~
+  +----------+                    +--------+                              +---------+
+  | Initial  | -- 'a'/action1 --> | stateA | -- [guard2]'b' / action2 --> | Final   |
+  +----------+                    +--------+                              +---------+
+~~~
+
+~~~
+  void action1() { std::cout << "perform custom action 1\n"; }
+  void action2() { std::cout << "perform custom action 2\n"; }
+  enum class States {Initial, A, Final};
+  using F = FSM::Fsm<States, States::Initial>;
+  std::vector<F::Trans> transitions =
+  {
+    // from state     , to state      , trigger, guard           , action
+    { States::Initial , States::A     , 'a'    , nullptr         , action1 },
+    { States::A       , States::Final , 'b'    , []{return true} , action2 },
+  };
+
+  F fsm;
+  fsm.add_transitions(transitions);
+  assert(is_initial());
+  fsm.execute('a');
+  assert(States::A == fsm.state());
+  fsm.execute('b');
+  assert(States::Final == fsm.state());
+  fsm.reset();
+  assert(is_initial());
+~~~
 */
 
 // Includes
